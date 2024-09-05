88b9a7edcebba45af22a564bd5b1ce2b6aa5d928
Alex Ant <dacom.dark.sun@gmail.com>
2024-09-05 19:27:23 +0500

    small letters

--- components/coopdoc-generator-ts/src/Templates/index.ts
+++
@@ -1,5 +0,0 @@
-export * as WalletAgreement from './1.WalletAgreement'
-export * as ParticipantApplication from './100.ParticipantApplication'
-export * as DecisionOfParticipantApplication from './501.DecisionOfParticipantApplication'
-
-export * from './registry'
