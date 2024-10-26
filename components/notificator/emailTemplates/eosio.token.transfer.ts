e7f896c38eef71331f2d982a736a6949dae10c6d
Alex Ant <dacom.dark.sun@gmail.com>
2024-10-26 10:49:28 +0500

    update notification template

--- components/notificator/emailTemplates/eosio.token.transfer.ts
+++ components/notificator/emailTemplates/eosio.token.transfer.ts
@@ -1,4 +1,4 @@
 export const notificationTemplate = {
-  subject: `{{#if (eq data.from receiver)}} Исходящий перевод на {{data.quantity}} {{else}} Входящий перевод на {{data.quantity}} {{/if}}`,
-  message: `{{#if (eq data.from receiver)}} Отправлен перевод на сумму {{data.quantity}} аккаунту {{data.to}} {{else}} Получен перевод на сумму {{data.quantity}} от {{data.from}} {{/if}}`,
+  subject: `{{#if (eq data.from receiver)}} Исходящий перевод на {{data.quantity}} от {{data.from}} на {{data.to}} {{else}} Входящий перевод на {{data.quantity}} от {{data.from}} на {{data.to}} {{/if}}`,
+  message: `{{#if (eq data.from receiver)}} Отправлен перевод на сумму {{data.quantity}} с аккаунта {{data.from}} на аккаунт {{data.to}} {{else}} Получен перевод на сумму {{data.quantity}} от {{data.from}} на аккаунт {{data.to}} {{/if}}`,
 }
