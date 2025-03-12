f46a1fb9b52525b11a9dcf3ebf8ae79e7f5bc2c1
Alex Ant <dacom.dark.sun@gmail.com>
2025-03-12 09:51:52 +0500

    ascii text fix

--- components/boot/src/index.ts
+++ components/boot/src/index.ts
@@ -132,7 +132,15 @@ program
       await runContainer()
       await checkHealth()
       await boot()
-      console.log('Boot process completed')
+      console.log(`
+ .d8888b.   .d88888b.  888b     d888 8888888b.  888      8888888888 88888888888 8888888888
+d88P  Y88b d88P" "Y88b 8888b   d8888 888   Y88b 888      888            888     888
+888    888 888     888 88888b.d88888 888    888 888      888            888     888
+888        888     888 888Y88888P888 888   d88P 888      8888888        888     8888888
+888        888     888 888 Y888P 888 8888888P"  888      888            888     888
+888    888 888     888 888  Y8P  888 888        888      888            888     888
+Y88b  d88P Y88b. .d88P 888   "   888 888        888      888            888     888
+ "Y8888P"   "Y88888P"  888       888 888        88888888 8888888888     888     8888888888`)
     }
     catch (error) {
       console.error('Failed to boot:', error)
--- components/setup/setup.ts
+++ components/setup/setup.ts
@@ -8,16 +8,14 @@ const execPromise = util.promisify(exec);
 
 console.clear();
 console.log(`
-  o          o        o__ __o        o          o        o__ __o      
- <|\        /|>      /v     v\      <|\        <|>      /v     v\     
- / \\o    o// \     />       <\     / \\o      / \     />       <\    
- \o/ v\  /v \o/   o/           \o   \o/ v\     \o/   o/           \o  
-  |   <\/>   |   <|             |>   |   <\     |   <|             |> 
- / \        / \   \\           //   / \    \o  / \   \\           //  
- \o/        \o/     \         /     \o/     v\ \o/     \         /    
-  |          |       o       o       |       <\ |       o       o     
- / \        / \      <\__ __/>      / \        < \      <\__ __/>             
-`);
+888b     d888  .d88888b.  888b    888  .d88888b.
+8888b   d8888 d88P" "Y88b 8888b   888 d88P" "Y88b
+88888b.d88888 888     888 88888b  888 888     888
+888Y88888P888 888     888 888Y88b 888 888     888
+888 Y888P 888 888     888 888 Y88b888 888     888
+888  Y8P  888 888     888 888  Y88888 888     888
+888   "   888 Y88b. .d88P 888   Y8888 Y88b. .d88P
+888       888  "Y88888P"  888    Y888  "Y88888P"`);
 
 interface MenuAnswers extends Answers {
   choice: 'dev' | 'use';
