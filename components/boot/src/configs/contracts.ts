bae45af414aeedfea36486c7a5482565f6dc9b2b
Alex Ant <dacom.dark.sun@gmail.com>
2025-03-12 09:33:47 +0500

    contracts path change

--- components/boot/src/configs/contracts.ts
+++ components/boot/src/configs/contracts.ts
@@ -7,72 +7,72 @@ config()
 export default [
   {
     name: 'eosio.boot',
-    path: path.resolve(process.cwd(), '../build/contracts/system/contracts/eosio.boot'),
+    path: path.resolve(process.cwd(), '../contracts/build/contracts/system/contracts/eosio.boot'),
     target: 'eosio',
   },
   {
     name: 'eosio.system',
-    path: path.resolve(process.cwd(), '../build/contracts/system/contracts/eosio.system'),
+    path: path.resolve(process.cwd(), '../contracts/build/contracts/system/contracts/eosio.system'),
     target: 'eosio',
   },
   {
     name: 'eosio.token',
-    path: path.resolve(process.cwd(), '../build/contracts/system/contracts/eosio.token'),
+    path: path.resolve(process.cwd(), '../contracts/build/contracts/system/contracts/eosio.token'),
     target: 'eosio.token',
   },
   {
     name: 'eosio.msig',
-    path: path.resolve(process.cwd(), '../build/contracts/system/contracts/eosio.msig'),
+    path: path.resolve(process.cwd(), '../contracts/build/contracts/system/contracts/eosio.msig'),
     target: 'eosio.msig',
   },
   {
     name: 'eosio.wrap',
-    path: path.resolve(process.cwd(), '../build/contracts/system/contracts/eosio.wrap'),
+    path: path.resolve(process.cwd(), '../contracts/build/contracts/system/contracts/eosio.wrap'),
     target: 'eosio.wrap',
   },
   {
     name: 'registrator',
-    path: path.resolve(process.cwd(), '../build/contracts/registrator'),
+    path: path.resolve(process.cwd(), '../contracts/build/contracts/registrator'),
     target: 'registrator',
   },
   {
     name: 'soviet',
-    path: path.resolve(process.cwd(), '../build/contracts/soviet'),
+    path: path.resolve(process.cwd(), '../contracts/build/contracts/soviet'),
     target: 'soviet',
   },
   {
     name: 'marketplace',
-    path: path.resolve(process.cwd(), '../build/contracts/marketplace'),
+    path: path.resolve(process.cwd(), '../contracts/build/contracts/marketplace'),
     target: 'marketplace',
   },
   {
     name: 'draft',
-    path: path.resolve(process.cwd(), '../build/contracts/draft'),
+    path: path.resolve(process.cwd(), '../contracts/build/contracts/draft'),
     target: 'draft',
   },
   {
     name: 'branch',
-    path: path.resolve(process.cwd(), '../build/contracts/branch'),
+    path: path.resolve(process.cwd(), '../contracts/build/contracts/branch'),
     target: 'branch',
   },
   {
     name: 'gateway',
-    path: path.resolve(process.cwd(), '../build/contracts/gateway'),
+    path: path.resolve(process.cwd(), '../contracts/build/contracts/gateway'),
     target: 'gateway',
   },
   {
     name: 'fund',
-    path: path.resolve(process.cwd(), '../build/contracts/fund'),
+    path: path.resolve(process.cwd(), '../contracts/build/contracts/fund'),
     target: 'fund',
   },
   {
     name: 'contributor',
-    path: path.resolve(process.cwd(), '../build/contracts/contributor'),
+    path: path.resolve(process.cwd(), '../contracts/build/contracts/contributor'),
     target: 'contributor',
   },
   {
     name: 'capital',
-    path: path.resolve(process.cwd(), '../build/contracts/capital'),
+    path: path.resolve(process.cwd(), '../contracts/build/contracts/capital'),
     target: 'capital',
   },
 ]
