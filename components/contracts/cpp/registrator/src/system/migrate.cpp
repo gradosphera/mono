986858c20dc2d16df56a7f71fc19dc9e0993002a
Alex Ant <dacom.dark.sun@gmail.com>
2025-04-29 11:05:03 +0500

    Обновление скриптов публикации и логики миграции. Изменены команды для публикации на продакшн, добавлен новый скрипт publish-prod.sh. Обновлена логика удаления кандидатов в функции миграции и исправлены ссылки на индексы кандидатов в confirmreg.cpp.

--- components/contracts/cpp/registrator/src/system/migrate.cpp
+++ components/contracts/cpp/registrator/src/system/migrate.cpp
@@ -1,10 +1,18 @@
 [[eosio::action]] void registrator::migrate() {
   require_auth(_registrator);
   
-  candidates_index candidates(_registrator, coopname.value);
-  auto it = candidates.begin();
-  while (it != candidates.end()) {
-    candidates.erase(it);
-    it = candidates.begin();
+  
+  cooperatives_index coops(_registrator, _registrator.value);
+
+  for (auto coop_it = coops.begin(); coop_it != coops.end(); ++coop_it) {
+      eosio::name coopname = coop_it->username;
+
+      Registrator::candidates_index candidates(_registrator, coopname.value);
+
+      auto it = candidates.begin();
+      while (it != candidates.end()) {
+        candidates.erase(it);
+        it = candidates.begin();
+      }
   }
 }
