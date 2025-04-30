777eb7e398920906a120c30b7a13be20eb43d251
Alex Ant <dacom.dark.sun@gmail.com>
2025-04-30 20:53:02 +0500

    Добавление middleware для инъекции переменных окружения в window.__ENV__ и рефакторинг функции получения переменных окружения с учетом различных сред (SSR, SPA). Теперь переменные окружения экспортируются через единую функцию getEnv().

---
+++ components/desktop/src-ssr/middlewares/injectEnv.js
@@ -0,0 +1,45 @@
+/**
+ * SSR middleware для инъекции переменных окружения в браузер
+ * Создает window.__ENV__ со всеми переменными, которые должны быть доступны на клиенте
+ */
+module.exports = function (/* { app, resolver, urlPath, publicPath, render } */) {
+  return async (req, res, next) => {
+    // Получаем переменные из process.env, которые нужны клиенту
+    const envForClient = {
+      NODE_ENV: process.env.NODE_ENV,
+      BACKEND_URL: process.env.BACKEND_URL,
+      CHAIN_URL: process.env.CHAIN_URL,
+      CHAIN_ID: process.env.CHAIN_ID,
+      CURRENCY: process.env.CURRENCY,
+      COOP_SHORT_NAME: process.env.COOP_SHORT_NAME,
+      SITE_DESCRIPTION: process.env.SITE_DESCRIPTION,
+      SITE_IMAGE: process.env.SITE_IMAGE,
+      STORAGE_URL: process.env.STORAGE_URL,
+      UPLOAD_URL: process.env.UPLOAD_URL,
+      VUE_ROUTER_MODE: process.env.VUE_ROUTER_MODE,
+      VUE_ROUTER_BASE: process.env.VUE_ROUTER_BASE
+    };
+
+    // Создаем скрипт, который добавит переменные в window.__ENV__
+    const script = `
+    <script>
+      window.__ENV__ = ${JSON.stringify(envForClient)};
+      console.log('SSR: Переменные окружения загружены');
+    </script>
+    `;
+
+    // Оригинальный метод отправки HTML
+    const originalSend = res.send;
+
+    // Переопределяем метод, чтобы вставить наш скрипт перед закрывающим тегом </head>
+    res.send = function (html) {
+      if (typeof html === 'string') {
+        html = html.replace('</head>', `${script}</head>`);
+      }
+      originalSend.call(this, html);
+    };
+
+    // Продолжаем обработку запроса
+    next();
+  };
+};
