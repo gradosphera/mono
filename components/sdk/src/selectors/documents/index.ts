edf7d9ab62c34bedef14adf067c0730735a892fe
Alex Ant <dacom.dark.sun@gmail.com>
2025-05-01 21:52:49 +0500

    docs #2

--- components/sdk/src/selectors/documents/index.ts
+++ components/sdk/src/selectors/documents/index.ts
@@ -1,4 +1,3 @@
 export * from '../agenda/documentPackageAggregateSelector'
 export * from './documentAggregateSelector'
-export * from './projectFreeDecisionSelector'
 export * from './signedBlockchainDocumentSelector'
