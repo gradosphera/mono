79b7ff21f55dc2d3bf1c67896a49fe70868cf2d9
Alex Ant <dacom.dark.sun@gmail.com>
2025-05-01 18:23:56 +0500

    Удалены отладочные логи из функций входа и выхода пользователя, а также обновлен способ проверки переменных окружения в функции работы с IndexedDB.

--- components/desktop/src/features/User/Logout/model/index.ts
+++ components/desktop/src/features/User/Logout/model/index.ts
@@ -8,15 +8,12 @@ export function useLogoutUser() {
   async function logout(): Promise<void> {
     const global = useGlobalStore()
     //TODO: "начать с начала" при регистрации бажит на это - да и в целом пора бы маршруты срезать эти
-    console.log('logout')
-    // if (global.tokens?.refresh.token) await api.logoutUser(global.tokens?.refresh.token)
     await useRegistratorStore().clearUserData()
 
     await global.logout()
 
     useSessionStore().close()
     useCurrentUserStore().clearAccount()
-    console.log('logout done')
   }
 
   return {
