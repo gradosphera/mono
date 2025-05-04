1b81cfc0a988dd2c92d591e8b53ef4e5931f077f
Alex Ant <dacom.dark.sun@gmail.com>
2025-05-04 12:42:23 +0500

    fix workflow #2

--- scripts/publish-prod.sh
+++ scripts/publish-prod.sh
@@ -10,27 +10,24 @@ LAST_TAG=$(git tag --list | grep "^$BASE_VERSION" | sort -V | tail -n 1)
 if [[ -z "$LAST_TAG" ]]; then
   VERSION="$BASE_VERSION"
 else
-  # Если есть точка после даты, увеличиваем суффикс
-  if [[ "$LAST_TAG" =~ ^$BASE_VERSION\.(\d+)$ ]]; then
-    SUFFIX=$(echo "$LAST_TAG" | awk -F. '{print $4}')
-    VERSION="$BASE_VERSION.$((SUFFIX+1))"
+  # Если есть дефис после даты, увеличиваем суффикс
+  if [[ "$LAST_TAG" =~ ^$BASE_VERSION-(\d+)$ ]]; then
+    SUFFIX=$(echo "$LAST_TAG" | awk -F- '{print $2}')
+    VERSION="$BASE_VERSION-$((SUFFIX+1))"
   else
     # Первый инкремент
-    VERSION="$BASE_VERSION.1"
+    VERSION="$BASE_VERSION-1"
   fi
 fi
 
 echo "Версионируем: $VERSION"
-
-# Убираем v для lerna
-LERNA_VERSION="${VERSION#v}"
 
 # Переходим на main и мержим testnet
 git checkout -f main
 git merge -X theirs testnet
 
 # Только версия и тег, без публикации
-lerna version "$LERNA_VERSION" --yes --no-push=false --no-git-tag-version=false --force-publish
+lerna version "$VERSION" --yes --no-push=false --no-git-tag-version=false --force-publish
 
 git push --follow-tags
 
