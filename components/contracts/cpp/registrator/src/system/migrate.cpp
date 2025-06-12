f38b78f821c5b1615c5566fe8c574c6a486bdb4d
Alex Ant <dacom.dark.sun@gmail.com>
2025-06-12 15:47:27 +0500

    подсчитываем количество активных пайщиков в кооперативах

--- components/contracts/cpp/registrator/src/system/migrate.cpp
+++ components/contracts/cpp/registrator/src/system/migrate.cpp
@@ -1,80 +1,35 @@
 [[eosio::action]] void registrator::migrate() {
   require_auth(_registrator);
-  // Registrator::candidates_legacy_index candidates_legacy(_registrator, _provider.value);
-  // auto candidate_legacy_itr = candidates_legacy.begin();
-  // while (candidate_legacy_itr != candidates_legacy.end()) {
-  //   candidate_legacy_itr = candidates_legacy.erase(candidate_legacy_itr);
-  // }
-
-
-
-  // Новый код: удаление всех записей из cooperatives2_index
-  // cooperatives2_index coops2(_registrator, _registrator.value);
-  // auto coop2_itr = coops2.begin();
-  // while (coop2_itr != coops2.end()) {
-  //   coop2_itr = coops2.erase(coop2_itr);
-  // }
   
-  cooperatives_index coops(_registrator, _registrator.value);
-  // Миграция cooperative -> cooperative2
+  // Получаем таблицу кооперативов
   cooperatives2_index coops2(_registrator, _registrator.value);
   
-  for (auto coop_itr = coops.begin(); coop_itr != coops.end(); ++coop_itr) {
-      // Проверяем, что запись еще не существует в cooperatives2
-      if (coops2.find(coop_itr->username.value) == coops2.end()) {
-          // Создаем новую запись в cooperative2
-          coops2.emplace(_registrator, [&](auto& new_coop) {
-              new_coop.username = coop_itr->username;
-              new_coop.parent_username = coop_itr->parent_username;
-              new_coop.announce = coop_itr->announce;
-              new_coop.description = coop_itr->description;
-              new_coop.is_cooperative = coop_itr->is_cooperative;
-              new_coop.is_branched = coop_itr->is_branched;
-              new_coop.is_enrolled = coop_itr->is_enrolled;
-              new_coop.coop_type = coop_itr->coop_type;
-              new_coop.registration = coop_itr->registration;
-              new_coop.initial = coop_itr->initial;
-              new_coop.minimum = coop_itr->minimum;
-              
-              if (coop_itr->org_registration.has_value())
-                  new_coop.org_registration = coop_itr->org_registration;
-              if (coop_itr->org_initial.has_value())
-                  new_coop.org_initial = coop_itr->org_initial;
-              if (coop_itr->org_minimum.has_value())
-                  new_coop.org_minimum = coop_itr->org_minimum;
-              if (coop_itr->status.has_value())
-                  new_coop.status = coop_itr->status;
-              if (coop_itr->created_at.has_value())
-                  new_coop.created_at = coop_itr->created_at;
-              
-              // Если есть документ в кооперативе, преобразуем его в document2
-              if (coop_itr->document.has_value()) {
-                  document2 new_doc;
-                  new_doc.version = "0";
-                  new_doc.hash = coop_itr->document.value().hash;
-                  new_doc.doc_hash = coop_itr->document.value().hash;
-                  new_doc.meta_hash = eosio::checksum256();
-                  new_doc.meta = coop_itr->document.value().meta;
-                  
-                  // Добавляем информацию о подписи
-                  signature_info sig;
-                  sig.id = 0;
-                  sig.signer = coop_itr->username;
-                  sig.public_key = coop_itr->document.value().public_key;
-                  sig.signature = coop_itr->document.value().signature;
-                  sig.signed_hash = coop_itr->document.value().hash;
-                  // Используем created_at как время подписания, если доступно
-                  if (coop_itr->created_at.has_value()) {
-                      sig.signed_at = coop_itr->created_at.value();
-                  } else {
-                      // В противном случае используем текущее время
-                      sig.signed_at = eosio::time_point_sec(eosio::current_time_point());
-                  }
-                  
-                  new_doc.signatures.push_back(sig);
-                  new_coop.document = new_doc;
-              }
-          });
+  // Для каждого кооператива проверяем наличие счетчика активных пайщиков
+  for (auto coop_itr = coops2.begin(); coop_itr != coops2.end(); ++coop_itr) {
+    // Если счетчик активных пайщиков уже установлен, пропускаем кооператив
+    if (coop_itr->active_participants_count.has_value()) {
+      continue;
+    }
+    
+    // Получаем имя кооператива для использования как scope
+    eosio::name coopname = coop_itr->username;
+    
+    // Получаем таблицу участников из контракта soviet с scope кооператива
+    participants_index participants(_soviet, coopname.value);
+    
+    // Счетчик активных пайщиков
+    uint64_t active_count = 0;
+    
+    // Проходим по всем участникам и считаем активных (has_vote == true)
+    for (auto part_it = participants.begin(); part_it != participants.end(); ++part_it) {
+      if (part_it->has_vote) {
+        active_count++;
       }
+    }
+    
+    // Обновляем запись кооператива, устанавливая счетчик активных пайщиков
+    coops2.modify(coop_itr, _registrator, [&](auto& coop) {
+      coop.active_participants_count = active_count;
+    });
   }
 }
