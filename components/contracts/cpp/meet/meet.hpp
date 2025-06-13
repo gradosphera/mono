9769cc0ed41ca883105cc82ff5bef855a6029085
Alex Ant <dacom.dark.sun@gmail.com>
2025-06-13 16:15:50 +0500

    enable test mode

--- components/contracts/cpp/meet/meet.hpp
+++ components/contracts/cpp/meet/meet.hpp
@@ -18,7 +18,7 @@ public:
     using contract::contract;
 
     static constexpr uint32_t MIN_OPEN_AGM_DELAY_SEC = 60 * 60 * 24 * 15; // 15 дней
-    static constexpr bool TEST_MODE = false; // Флаг тестового режима
+    static constexpr bool TEST_MODE = true; // Флаг тестового режима
 
     [[eosio::action]]
     void createmeet(name coopname, checksum256 hash, eosio::name initiator, name presider, name secretary, std::vector<meet_point> agenda, document2 proposal, time_point_sec open_at, time_point_sec close_at);
