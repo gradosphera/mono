414126710402c37224dc2e1960e1a20e02ba38cd
Alex Ant <dacom.dark.sun@gmail.com>
2025-11-28 13:05:04 +0500

    disable unused routes

--- components/controller/src/routes/v1/system.route.ts
+++ components/controller/src/routes/v1/system.route.ts
@@ -1,30 +1,8 @@
 import { Router } from 'express';
-import { settingController, systemController } from '../../controllers';
-import auth from '../../middlewares/auth';
-import validate from '../../middlewares/validate';
-import { systemValidation } from '../../validations';
+import { systemController } from '../../controllers';
 
 const router = Router();
 
-//init - устанавливает данные организации кооператива
-router.route('/init').post(auth('init'), validate(systemValidation.RInit), systemController.init);
-
-router.route('/install').post(validate(systemValidation.RInstall), systemController.install);
-
-router.route('/wif').post(auth('setWif'), validate(systemValidation.RSetWif), systemController.setWif);
-
-//wif
-
-router.route('/vars/schema').get(auth('getVars'), systemController.getVarsSchema);
-router.route('/vars').get(auth('getVars'), systemController.getVars);
-router.route('/vars').post(auth('setVars'), validate(systemValidation.RSetVars), systemController.setVars);
-
 router.route('/health').get(systemController.getHealth);
-
-router.route('/settings').get(auth('manageSettings'), settingController.getSettings);
-
-router
-  .route('/settings')
-  .post(auth('manageSettings'), validate(systemValidation.RUpdateSettings), settingController.updateSettings);
 
 export default router;
