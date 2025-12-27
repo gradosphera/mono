9d72a5e02d6390c9bd49aac988c2a26130ac9a0c
Alex Ant <dacom.dark.sun@gmail.com>
2025-12-27 20:21:56 +0500

    openScreen

--- components/desktop/src/boot/sentry.ts
+++ components/desktop/src/boot/sentry.ts
@@ -2,25 +2,25 @@ import { boot } from 'quasar/wrappers';
 import { App } from 'vue';
 import { Router } from 'vue-router';
 import { env } from 'src/shared/config';
-// import Tracker from '@openreplay/tracker';
-// import trackerAssist from '@openreplay/tracker-assist'
-// import { useSessionStore } from 'src/entities/Session';
+import Tracker from '@openreplay/tracker';
+import trackerAssist from '@openreplay/tracker-assist'
+import { useSessionStore } from 'src/entities/Session';
 export default boot(
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   ({ app, router }: { app: App<Element>; router: Router }) => {
-    if ((env.NODE_ENV as string) === 'production') {
-      // const session = useSessionStore()
+    if (process.env.CLIENT&& (env.NODE_ENV as string) === 'production') {
+      const session = useSessionStore()
 
-      // const tracker = new Tracker({
-      //   projectKey: 'mgaCVSShnDNbPRFDZehd',
-      // });
+      const tracker = new Tracker({
+        projectKey: 'mgaCVSShnDNbPRFDZehd',
+      });
 
-      // if (session.username)
-      //   tracker.setUserID(session.username)
+      if (session.username)
+        tracker.setUserID(session.username)
 
-      // // .start() returns a promise
-      // tracker.start().then(sessionData => console.log(sessionData) ).catch(e => console.error('on tracker: ', e) )
-      // tracker.use(trackerAssist({}));
+      // .start() returns a promise
+      tracker.start().then(sessionData => console.log(sessionData) ).catch(e => console.error('on tracker: ', e) )
+      tracker.use(trackerAssist({}));
     }
   }
 );
