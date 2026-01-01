0c2d5679f156e4895ba3c53d9bcf40c9c7f42276
Alex Ant <dacom.dark.sun@gmail.com>
2026-01-01 19:05:12 +0500

    extensions reinit

--- components/controller/src/domain/extension/extension-domain.module.ts
+++ components/controller/src/domain/extension/extension-domain.module.ts
@@ -5,6 +5,7 @@ import { ExtensionDomainService } from './services/extension-domain.service';
 import { ExtensionLifecycleDomainService } from '~/domain/extension/services/extension-lifecycle-domain.service';
 import { ExtensionDomainListingService } from './services/extension-listing-domain.service';
 import { ExtensionSchemaMigrationService } from './services/extension-schema-migration.service';
+import { ExtensionInteractor } from '~/application/appstore/interactors/extension.interactor';
 import { powerupSchemaV2Migration } from '~/extensions/powerup/migrations/powerup-schema-v2.migration';
 import { WinstonLoggerService } from '~/application/logger/logger-app.service';
 
@@ -22,6 +23,7 @@ import { ExtensionPortsModule } from './extension-ports.module';
     ExtensionLifecycleDomainService,
     ExtensionDomainListingService,
     ExtensionSchemaMigrationService,
+    ExtensionInteractor,
   ],
   exports: [
     ExtensionDomainService,
@@ -35,13 +37,24 @@ export class ExtensionDomainModule {
   constructor(
     private readonly extensionLifecycleDomainService: ExtensionLifecycleDomainService,
     private readonly migrationService: ExtensionSchemaMigrationService,
+    private readonly extensionInteractor: ExtensionInteractor,
     private readonly logger: WinstonLoggerService
   ) {}
 
   async onModuleInit() {
+    this.logger.info('[EXTENSION_INIT] Начинаем инициализацию системы расширений');
+
     // Регистрируем миграции схем расширений
     this.migrationService.registerMigration(powerupSchemaV2Migration);
 
+    // Устанавливаем расширения по умолчанию
+    await this.extensionInteractor.installDefaultApps();
+
     this.extensionLifecycleDomainService.setAppContext(nestApp);
+
+    // Запускаем инициализацию включенных расширений
+    await this.extensionLifecycleDomainService.runApps();
+
+    this.logger.info('[EXTENSION_INIT] Инициализация системы расширений завершена успешно');
   }
 }
