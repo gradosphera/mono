a556c29a1711ccbc82c6df2ede61ad34b538bf3f
Alex Ant <dacom.dark.sun@gmail.com>
2026-02-20 00:12:02 +0500

    fix docker base image

---
+++ components/migrator/migrations/043_edit_selected_draft.ts
@@ -0,0 +1,57 @@
+import { Cooperative, DraftContract, SovietContract } from 'cooptypes';
+import { Registry } from '@coopenomics/factory';
+import type { Migration } from '../src/migration_interface';
+import { api } from '../src/eos';
+import { editDraft } from '../src/utils/editDraft';
+import { editTranslation } from '../src/utils/editTranslation';
+
+export class InitialMigration implements Migration {
+  async run(): Promise<void> {
+    const ids = [1000, 999, 998]; 
+
+    for (const id of ids) {
+      try {
+        const drafts = await api.rpc.get_table_rows({
+          json: true,
+          code: 'draft',
+          scope: 'draft',
+          table: 'drafts',
+          limit: 1000
+        });
+
+        const target = drafts.rows.find((draft) => draft.registry_id === id);
+        if (!target) {
+          console.warn(`Draft with registry_id ${id} not found.`);
+          continue;
+        }
+
+        const template = Registry[id as unknown as keyof typeof Registry];
+        if (!template) {
+          console.warn(`Template for registry_id ${id} not found in Registry.`);
+          continue;
+        }
+
+        await editDraft({
+          scope: DraftContract.contractName.production,
+          username: 'eosio',
+          registry_id: id,
+          title: template.Template.title,
+          description: template.Template.description,
+          context: template.Template.context,
+          model: JSON.stringify(template.Template.model),
+        });
+
+        await editTranslation({
+          scope: DraftContract.contractName.production,
+          username: 'eosio',
+          translate_id: target.default_translation_id,
+          data: JSON.stringify(template.Template.translations.ru),
+        });
+
+        console.log(`Update for registry_id ${id} successful`);
+      } catch (error) {
+        console.error(`Error during migration for registry_id ${id}:`, error);
+      }
+    }
+  }
+}
---
+++ components/migrator/migrations/044_create_drafts.ts
@@ -0,0 +1,34 @@
+import { Cooperative, DraftContract, SovietContract } from 'cooptypes';
+import { Registry } from '@coopenomics/factory';
+import type { Migration } from '../src/migration_interface';
+import { api } from '../src/eos';
+import { createDraft } from '../src/utils/createDraft';
+
+export class InitialMigration implements Migration {
+  async run(): Promise<void> {
+    const ids = [994, 995, 996, 997, 1001, 1002, 1003, 1004, 1005, 1006, 1007, 1020, 1040, 1041, 1042];
+    for (const id of ids) {
+      try {
+        const template = Registry[id as unknown as keyof typeof Registry];
+        if (!template) {
+          console.warn(`Template for registry_id ${id} not found in Registry.`);
+          continue;
+        }
+        await createDraft({
+          scope: DraftContract.contractName.production,
+          username: 'eosio',
+          registry_id: id,
+          lang: 'ru',
+          title: template.Template.title,
+          description: template.Template.description,
+          context: template.Template.context,
+          model: JSON.stringify(template.Template.model),
+          translation_data: JSON.stringify(template.Template.translations.ru),
+        });
+        console.log(`Draft for registry_id ${id} created successfully`);
+      } catch (error) {
+        console.error(`Error during migration for registry_id ${id}:`, error);
+      }
+    }
+  }
+}
