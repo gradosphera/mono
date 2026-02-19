fe0243549a78edd72496e8cd89f493d6765724da
Alex Ant <dacom.dark.sun@gmail.com>
2026-02-19 23:07:12 +0500

    update .env-example

--- components/desktop/src/pages/ExtensionStore/ExtensionsShowcase/ExtensionsShowcase.vue
+++ components/desktop/src/pages/ExtensionStore/ExtensionsShowcase/ExtensionsShowcase.vue
@@ -1,15 +1,25 @@
 <template lang="pug">
 div.row
-  div(v-for="extension in extStore.extensions" v-bind:key="extension.name").col-md-3.col-sm-6.col-xs-12
+  div(v-for="extension in filteredExtensions" v-bind:key="extension.name").col-md-3.col-sm-6.col-xs-12
     ExtensionCard(:extension="extension")
 
 </template>
 <script lang="ts" setup>
 import { useExtensionStore } from 'src/entities/Extension/model';
-import { onMounted } from 'vue';
+import { useSystemStore } from 'src/entities/System/model';
+import { onMounted, computed } from 'vue';
 import { ExtensionCard } from 'src/widgets/ExtensionCard';
 
 const extStore = useExtensionStore()
+const systemStore = useSystemStore()
+
+const filteredExtensions = computed(() => {
+  // Временно: скрываем расширение 'capital' для всех кооперативов кроме 'voskhod'
+  // TODO: убрать фильтрацию потом, когда станет доступно для всех
+  return extStore.extensions.filter((extension) => 
+    extension.name !== 'capital' || systemStore.info.coopname === 'voskhod'
+  )
+})
 
 onMounted(async () => {
   extStore.loadExtensions()
