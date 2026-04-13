9deee2e552d0b5e5cc092b66e6ff9cfde41b6c7f
Alex Ant <dacom.dark.sun@gmail.com>
2026-04-13 14:21:37 +0500

    capital: migration complete

--- components/contracts/cpp/capital/app/contract_managment/migrate/migrate.cpp
+++ components/contracts/cpp/capital/app/contract_managment/migrate/migrate.cpp
@@ -12,34 +12,4 @@
 void capital::migrate() {
   require_auth(_capital);
 
-  const eosio::name coopname = "voskhod"_n;
-
-  Capital::contributor_index contributors(_capital, coopname.value);
-
-  std::vector<uint64_t> contributor_ids;
-  contributor_ids.reserve(32);
-  for (auto itr = contributors.begin(); itr != contributors.end(); ++itr) {
-    contributor_ids.push_back(itr->id);
-  }
-
-  for (uint64_t contributor_id : contributor_ids) {
-    auto itr = contributors.find(contributor_id);
-    if (itr == contributors.end()) {
-      continue;
-    }
-
-    eosio::asset wallet_total = eosio::asset(0, _root_govern_symbol);
-    auto pw_opt = Capital::Wallets::get_program_capital_wallet(coopname, itr->username);
-    if (pw_opt.has_value()) {
-      const progwallet &pw = pw_opt.value();
-      eosio::asset blocked_part = pw.blocked.has_value()
-                                      ? pw.blocked.value()
-                                      : eosio::asset(0, _root_govern_symbol);
-      wallet_total = pw.available + blocked_part;
-    }
-
-    contributors.modify(itr, _capital, [&](auto &c) {
-      c.contributed_as_investor = wallet_total;
-    });
-  }
 }
