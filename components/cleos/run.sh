9b6cadb3be766e65d24dee79026b824700854fe2
coopops <coopos@coopenomics.world>
2026-05-01 06:56:19 +0000

    update blockchain to latest tag

--- components/cleos/run.sh
+++ components/cleos/run.sh
@@ -2,5 +2,5 @@ docker run --rm -it --name cleos \
   --volume ./eosio-wallet:/root/eosio-wallet \
   --volume ./scripts:/root/scripts \
   -w /root/scripts \
-  dicoop/blockchain_v5.1.1:dev /bin/bash
+  dicoop/blockchain:latest /bin/bash
 
